35a4200afd6c35bc61a473b754efecc5080f5e50
SakshiNehe <sakshinehe22@gmail.com>
2025-02-18 20:36:00 +0530

    chnages in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "./Components";
-import Part1 from "./Components/Part1";
-import Plans from "./Components/Plans";
-import HowItWorks from "./Components/HowItWorks";
-import WhyInsureEase from "./Components/WhyInsureEase";
-import CoreFunc from "./Components/CoreFunc";
-import Calculators from "./Components/Calculators";
-import FAQ from "./Components/FAQ";
-import Footer from "./Components/Footer";
-
-// Importing all individual insurance pages
+import './App.css'
+import Home from './pages/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createRoot } from 'react-dom/client'
+import { Login, Register } from './Components'
+import CarInsuranceCalculator from './Components/CarInsuranceCalculator';
+import FixedDepositCalculator from './Components/FixedDepositCalculator';
 import CarInsurance from "./pages/car";
 import TravelInsurance from "./pages/travel";
-import HomeInsurance from "./pages/home";
+import HomeInsurance from "./pages/homeInc";
 import HealthInsurance from "./pages/health";
 import LifeInsurance from "./pages/termlifeinsurance";
 import BikeInsurance from "./pages/bike";
@@ -22,48 +17,95 @@ import InvestmentInsurance from "./pages/invest";
 import EmployeeHealthInsurance from "./pages/employeehealth";
 import ChildSavingsInsurance from "./pages/childsaving";
 import RetirementInsurance from "./pages/retirementinsurance";
-
-
-
-
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        {/* Main Page */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Part1 />
-              <Plans />
-              <HowItWorks />
-              <WhyInsureEase />
-              <CoreFunc />
-              <Calculators />
-              <FAQ />
-            </>
-          }
-        />
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: "",
+      children: [
+        {
+          path: "",
+          element: <Home />
+        },
+        {
+          path: "login",
+          element: <Login />
+        },
+        {
+          path: "register",
+          element: <Register />
+        },
+        {
+          path: "/fixed-deposit-calculator",  // Route for FD Calculator
+          element: <FixedDepositCalculator />,
+        },
+        {
+          path: "/car-insurance-calculator",  // Route for Car Insurance Calculator
+          element: <CarInsuranceCalculator />,
+        },
+        {
+          path: "/insurance/car",  
+          element: <CarInsurance/>,
+        },
+        {
+          path: "/insurance/travel",  
+          element: <TravelInsurance/>,
+        },
+        {
+          path: "/insurance/home",  
+          element:<HomeInsurance /> ,
+        },
+        {
+          path: "/insurance/health",  
+          element:<HealthInsurance /> ,
+        },
+        {
+          path: "/insurance/term-life",  
+          element:<LifeInsurance /> ,
+        },
+        {
+          path: "/insurance/bike",  
+          element:<BikeInsurance/>,
+        },
+        {
+          path: "/insurance/family-health",  
+          element:<FamilyHealthInsurance/>,
+        },
+        {
+          path: "/insurance/term-women",  
+          element:<TermWomenInsurance/>,
+        },
+        {
+          path: "/insurance/term-women",  
+          element:<TermWomenInsurance/>,
+        },
+        {
+          path: "/insurance/investment",  
+          element:<InvestmentInsurance/>,
+        },
+        {
+          path: "/insurance/employee-health",  
+          element:<EmployeeHealthInsurance/>,
+        },
+        {
+          path: "/insurance/child-saving",  
+          element:<ChildSavingsInsurance/>,
+        },
+        {
+          path: "/insurance/retirement",  
+          element:<RetirementInsurance/>,
+        },
+        
 
-        {/* Insurance Pages */}
-        <Route path="/insurance/car" element={<CarInsurance />} />
-        <Route path="/insurance/travel" element={<TravelInsurance />} />
-        <Route path="/insurance/home" element={<HomeInsurance />} />
-        <Route path="/insurance/health" element={<HealthInsurance />} />
-        <Route path="/insurance/term-life" element={<LifeInsurance />} />
-        <Route path="/insurance/bike" element={<BikeInsurance />} />
-        <Route path="/insurance/family-health" element={<FamilyHealthInsurance />} />
-        <Route path="/insurance/term-women" element={<TermWomenInsurance/>} />
-        <Route path="/insurance/investment" element={<InvestmentInsurance />} />
-        <Route path="/insurance/employee-health" element={<EmployeeHealthInsurance />} />
-        <Route path="/insurance/child-savings" element={<ChildSavingsInsurance />} />
-        <Route path="/insurance/retirement" element={<RetirementInsurance />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+      ]
+    }
+  ])
+  return (
+    <RouterProvider router={router}>
+      <Home />
+    </RouterProvider>
+
+  )
 }
 
 export default App;
